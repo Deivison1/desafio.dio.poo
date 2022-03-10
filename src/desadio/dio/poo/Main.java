dc7b6b2e3010e2769ddb078af22680454a8cfa8b
Deivison1 <santosdeivison480@gmail.com>
2022-03-10 10:48:45 -0300

    entendendo herança e polimorfismo

--- src/desadio/dio/poo/Main.java
+++ src/desadio/dio/poo/Main.java
@@ -2,7 +2,10 @@ package desadio.dio.poo;
 
 import java.time.LocalDate;
 
+import desadio.dio.poo.dominio.Bootcamp;
+import desadio.dio.poo.dominio.Conteudo;
 import desadio.dio.poo.dominio.Curso;
+import desadio.dio.poo.dominio.Dev;
 import desadio.dio.poo.dominio.Mentoria;
 
 public class Main {
@@ -24,10 +27,40 @@ public class Main {
 		mentoria.setDescricao("Descrição da mentoria");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso1);
+		/*System.out.println(curso1);
 		System.out.println(curso2);
-		System.out.println(mentoria);
-
+		System.out.println(mentoria);*/
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Boootcamp java Developer");
+        bootcamp.setDescricao("Decrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+        
+        Dev devDeivison = new Dev();
+        devDeivison.setNome("Deivison");
+        devDeivison.inscreverBootcamp(bootcamp);
+        devDeivison.progredir();
+        
+        
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Deivison: " + devDeivison.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Deivison: " + devDeivison.getConteudosConcluidos());
+        System.out.println("Xp: " + devDeivison.calcularTotalXp());
+        System.out.println("-----------");
+        
+        Dev devPedro = new Dev();
+        devPedro.inscreverBootcamp(bootcamp);
+        devPedro.progredir();
+        
+        devPedro.setNome("Pedro");
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Pedro: " + devPedro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Pedro: " + devPedro.getConteudosConcluidos());
+        System.out.println("Xp: " + devPedro.calcularTotalXp());
+        System.out.println("-----------");
+        
 	}
 
 }
--- src/desadio/dio/poo/dominio/Conteudo.java
+++ src/desadio/dio/poo/dominio/Conteudo.java
@@ -1,5 +1,35 @@
 package desadio.dio.poo.dominio;
 
-public class Conteudo {
+public abstract class Conteudo {
 
+	protected static final double XP_PADRAO = 20d;
+	
+	private String titulo;
+	private String descricao;
+	
+	public abstract double calcularXp();
+	
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public static double getXpPadrao() {
+		return XP_PADRAO;
+	} 
+	
+	
+	
 }
